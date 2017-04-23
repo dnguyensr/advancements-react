5c600c61b04fbec57feb004085872192b1b35c09
Daniel <danielnguyensr@gmail.com>
2017-04-22 18:51:15 -0700

    Add, install, config devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#index'
   PagesController.action_methods.each do |action|
       get "/#{action}", to: "pages##{action}", as: "#{action}_page"
