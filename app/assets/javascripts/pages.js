61c896c9588e5c4fc822e2d510fa441239ccae34
Daniel <danielnguyensr@gmail.com>
2017-05-12 22:35:28 -0700

    Add event listeners for list of merit badges

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+document.addEventListener("turbolinks:load", function() {
+  var badges = document.querySelector('.badge-list');
+  badges.addEventListener('click', function handleClick(e) {
+    if (e.target.dataset.id != undefined) {
+      if (e.target.dataset.complete === false) {
+        console.log("Implement addMeritBadge")
+        console.log({id: e.target.dataset.id, complete: e.target.dataset.complete});
+      } else {
+        console.log("Implement removeMeritBadge")
+        console.log({id: e.target.dataset.id, complete: e.target.dataset.complete});
+      }
+    }
+  })
+
+});
